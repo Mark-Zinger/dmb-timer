d316750c5378815e43f7afc98595facf043c13e3
Mark Zinger <markzingergithub@gmail.com>
2021-02-03 14:16:36 +0900

    fix

--- publish.js
+++ publish.js
@@ -1,5 +1,6 @@
 const ghpages = require('gh-pages');
 
 ghpages.publish('build', {
-    message: 'Auto-generated commit'
+    message: 'Auto-generated commit',
+    branch: 'publish'
 }, () => console.log('publish success'));
